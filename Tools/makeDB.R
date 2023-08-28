dc0f7e253561c27f55d02cb1f91c247376c60072
Heta Desai <45779381+hdesai17@users.noreply.github.com>
2023-08-28 13:58:04 -0700

    Update makeDB.R

--- Tools/makeDB.R
+++ Tools/makeDB.R
@@ -221,5 +221,13 @@ output_file <- paste("2TS_", sample_name, "_edited_simple_dedup_rev_upper.fa", s
 command <- paste("dd if=", input_file, " of=", output_file, " conv=ucase", sep = "")
 # Execute the shell command
 system(command)
+
+setwd(db_directory)
+# Construct the shell command
+input_file <- paste(sample_name, "_custom.fa", sep = "")
+output_file <- paste(sample_name, "_custom_upper.fa", sep = "")
+command <- paste("dd if=", input_file, " of=", output_file, " conv=ucase", sep = "")
+# Execute the shell command
+system(command)
 print("Done")
 print("Finished database generation, outputs are in Custom_Databases folder")
